e37433d56d91cf4d895d6fa7bd4992dd79c2ea0d
Daniel Maldonado <damarias991@gmail.com>
2023-08-13 07:47:44 -0500

    13/08: def get_valid_words created

---
+++ hangman.py
@@ -0,0 +1,13 @@
+# Import elements & libraries
+import random
+from words import words_list
+
+# Random selection of VALID words
+
+def get_valid_word(words_list):
+    random_word = random.choice(words_list)
+    while '-' in random_word or ' ' in random_word:
+        random_word = random.choice(words_list)
+    return random_word
+
+print(get_valid_word(words_list))
