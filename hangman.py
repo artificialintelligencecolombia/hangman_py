93d79b370a757ce191162359c43c3983b30ef6fa
Daniel Maldonado <damarias991@gmail.com>
2023-08-17 06:41:10 -0500

    17/08: character selection conditional (with error)

--- hangman.py
+++ hangman.py
@@ -1,21 +1,36 @@
 # Import elements & libraries
 import random
+import string
 from words import words_list
 
 # Random selection of valid word from words_list's dataset
 def get_valid_word(words_list):
     random_word = random.choice(words_list) #Randomly chooses something from the list
     
-    #if the word contains spaces or "-", keep searching for another word.
+    # If the word contains spaces or "-", keep searching for another word.
     while '-' in random_word or ' ' in random_word:
         random_word = random.choice(words_list)
     return random_word.upper()
 
+
 def hangman():
     target_word = get_valid_word(words_list) 
     target_word_letters = set(target_word)
-    print(target_word, target_word_letters) 
-    return target_word_letters
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() #Empty set for the burnt letters
+    
+    # Get users input
+    user_bet = input("Guess a letter").upper()
+    if(user_bet in alphabet - used_letters):
+        used_letters.add(user_bet)
+        target_word_letters.remove(user_bet)
+    elif(user_bet in used_letters):
+        print("This letter is already used. Try again.")
+    else:
+        print("Invalid value. Try again.")
+    
+    #print(target_word, target_word_letters) 
+    
 
 
 
